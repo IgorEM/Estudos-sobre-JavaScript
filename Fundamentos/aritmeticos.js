bf9648efc5c220e23c172a4aa42e8167d36e63cd
Igor Eduardo <50914198+M0oses@users.noreply.github.com>
2020-04-02 22:00:23 -0300

    Fundamentos do JS

---
+++ Fundamentos/aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = a - d
+const mult = c * b
+const div = d / b
+const modulo = a%2 // resto = 0 é par
+console.log(soma)
+console.log(subtracao)
+console.log(mult)
+console.log(div)
+console.log(modulo)
+console.log(soma, subtracao, mult, -div, modulo)
